cd4c7edce76274174ea790fd2017c327f9b53d3b
mikaelvaaraniemi <mikael.vaaraniemi@bmw.de>
2021-08-09 21:21:41 +0200

    Initial commit of starter code

--- MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,51 +17,8 @@
 
 package com.example.android.marsrealestate.network
 
-// import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-// import kotlinx.coroutines.Deferred
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
-
 private const val BASE_URL = "https://mars.udacity.com/"
-enum class MarsApiFilter(val value: String) { SHOW_RENT("rent"), SHOW_BUY("buy"), SHOW_ALL("all") }
 
-/**
- * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
- * full Kotlin compatibility.
- */
-private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
-
-/**
- * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
- * object.
- */
-private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .baseUrl(BASE_URL)
-        .build()
-
-/**
- * A public interface that exposes the [getProperties] method
- */
-interface MarsApiService {
-    /**
-     * Returns a Coroutine [List] of [MarsProperty] which can be fetched with await() if in a Coroutine scope.
-     * The @GET annotation indicates that the "realestate" endpoint will be requested with the GET
-     * HTTP method
-     */
-    @GET("realestate")
-    suspend fun getProperties(@Query("filter") type: String): List<MarsProperty>
-}
-
-/**
- * A public Api object that exposes the lazy-initialized Retrofit service
- */
-object MarsApi {
-    val retrofitService : MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
-}
+// TODO (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
+// TODO (03) Implement the MarsApiService interface with @GET getProperties returning a String
+// TODO (04) Create the MarsApi object using Retrofit to implement the MarsApiService
--- MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,94 +20,31 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.android.marsrealestate.network.MarsApi
-import com.example.android.marsrealestate.network.MarsApiFilter
-import com.example.android.marsrealestate.network.MarsProperty
-//import kotlinx.coroutines.CoroutineScope
-//import kotlinx.coroutines.Dispatchers
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
 
-enum class MarsApiStatus { LOADING, ERROR, DONE }
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData that stores the status of the most recent request
-    private val _status = MutableLiveData<MarsApiStatus>()
+    // The internal MutableLiveData String that stores the status of the most recent request
+    private val _response = MutableLiveData<String>()
 
-    // The external immutable LiveData for the request status
-    val status: LiveData<MarsApiStatus>
-        get() = _status
-
-    // Internally, we use a MutableLiveData, because we will be updating the List of MarsProperty
-    // with new values
-    private val _properties = MutableLiveData<List<MarsProperty>>()
-
-    // The external LiveData interface to the property is immutable, so only this class can modify
-    val properties: LiveData<List<MarsProperty>>
-        get() = _properties
-
-    // Internally, we use a MutableLiveData to handle navigation to the selected property
-    private val _navigateToSelectedProperty = MutableLiveData<MarsProperty>()
-
-    // The external immutable LiveData for the navigation property
-    val navigateToSelectedProperty: LiveData<MarsProperty>
-        get() = _navigateToSelectedProperty
-
-
+    // The external immutable LiveData for the request status String
+    val response: LiveData<String>
+        get() = _response
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
     init {
-        getMarsRealEstateProperties(MarsApiFilter.SHOW_ALL)
+        getMarsRealEstateProperties()
     }
 
     /**
-     * Gets filtered Mars real estate property information from the Mars API Retrofit service and
-     * updates the [MarsProperty] [List] and [MarsApiStatus] [LiveData]. The Retrofit service
-     * returns a coroutine Deferred, which we await to get the result of the transaction.
-     * @param filter the [MarsApiFilter] that is sent as part of the web server request
+     * Sets the value of the status LiveData to the Mars API status.
      */
-     private fun getMarsRealEstateProperties(filter: MarsApiFilter) {
-        viewModelScope.launch {
-            _status.value = MarsApiStatus.LOADING
-            try {
-                _properties.value = MarsApi.retrofitService.getProperties(filter.value)
-                _status.value = MarsApiStatus.DONE
-            } catch (e: Exception) {
-                _status.value = MarsApiStatus.ERROR
-                _properties.value = ArrayList()
-            }
-        }
-    }
-
-    /**
-     */
-
-    /**
-     * When the property is clicked, set the [_navigateToSelectedProperty] [MutableLiveData]
-     * @param marsProperty The [MarsProperty] that was clicked on.
-     */
-    fun displayPropertyDetails(marsProperty: MarsProperty) {
-        _navigateToSelectedProperty.value = marsProperty
-    }
-
-    /**
-     * After the navigation has taken place, make sure navigateToSelectedProperty is set to null
-     */
-    fun displayPropertyDetailsComplete() {
-        _navigateToSelectedProperty.value = null
-    }
-
-    /**
-     * Updates the data set filter for the web services by querying the data with the new filter
-     * by calling [getMarsRealEstateProperties]
-     * @param filter the [MarsApiFilter] that is sent as part of the web server request
-     */
-    fun updateFilter(filter: MarsApiFilter) {
-        getMarsRealEstateProperties(filter)
+    private fun getMarsRealEstateProperties() {
+        // TODO (05) Call the MarsApi to enqueue the Retrofit request, implementing the callbacks
+        _response.value = "Set the Mars API Response here!"
     }
 }
