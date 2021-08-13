65a18f512797f05edb8e526a48e8b122578ea778
mikaelvaaraniemi <mikael.vaaraniemi@bmw.de>
2021-08-13 23:59:50 +0200

    lesson 26 - added Chips and ChipGroups

--- GDG-Finder/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
+++ GDG-Finder/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
@@ -7,12 +7,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.android.gdgfinder.R
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 
 private const val LOCATION_PERMISSION_REQUEST = 1
@@ -54,6 +56,37 @@ class GdgListFragment : Fragment() {
                         Snackbar.LENGTH_LONG
                     ).show()
                 }
+            }
+        })
+
+
+        viewModel.regionList.observe(viewLifecycleOwner, object:Observer<List<String>> {
+            override fun onChanged(data: List<String>?) {
+                data ?: return
+                // make new chip view for each item of the list
+                val chipGroup = binding.regionsList
+                val inflator = LayoutInflater.from(chipGroup.context)
+
+                // iterate data and call inflator for each item
+                val children = data.map { regionName ->
+                    val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+                    chip.text = regionName
+                    chip.tag = regionName
+                    chip.setOnCheckedChangeListener { button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
+                }
+
+                // remove any views already in the chip-group
+                chipGroup.removeAllViews()
+
+                // add the new children to the chip-group
+                for (chip in children ) {
+                    chipGroup.addView(chip)
+                }
+
+
             }
         })
 
